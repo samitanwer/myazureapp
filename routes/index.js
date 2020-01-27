0da888e1e9dffc6a5e41741b9aa065d55ad62c6a
unknown <samitxtreme@gmail.com>
2020-01-27 09:36:52 +0000

    staging deployment

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Azure' });
+//  res.render('index', { title: 'Azure' });
+var model = {
+  title : 'Azure', 
+  message: process.env.MESSAGE || "This is development env" 
+};
+res.render('index', model);
 });
 
 module.exports = router;
